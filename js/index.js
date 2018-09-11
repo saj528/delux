8b88cdd94459d337b586298c056662223c0b329d
Alex <ajmarksboy@gmail.com>
2018-09-11 17:57:38 -0500

    scene 2

--- js/index.js
+++ js/index.js
@@ -20,8 +20,10 @@ gameScene.preload = function() {
   this.load.image('billboardrandom', 'assets/billboardrandom.png');
   this.load.image('billboardvampire', 'assets/billboardvampire.png');
   this.load.image('skeleton', 'assets/skeleton.png');
+  this.load.image('sign1', 'assets/sign1.png');
   this.load.image('microtable', 'assets/microtable.png');
   this.load.image('guy', 'assets/guy.png');
+  this.load.image('sentra', 'assets/sentra.png');
   this.load.image('xterra', 'assets/xterra.png');
   this.load.image('bubble', 'assets/bubble.png');
   this.load.image('button', 'assets/button.png');
@@ -46,6 +48,12 @@ gameScene.preload = function() {
         frameHeight: 200,
       }
   );
+  this.load.spritesheet('pikachu',
+      'assets/pikachu.png',
+      { frameWidth: 40,
+        frameHeight: 39,
+      }
+  );
 };
 
 gameScene.create = function() {
@@ -59,6 +67,7 @@ gameScene.create = function() {
   this.mountains.scrollFactorX = 0;
   this.plantsBack = this.add.tileSprite(400, 279, 800, 600, 'plantsback');
   this.plantsBack.scrollFactorX = 0;
+  this.sign = this.add.sprite(1800, 100, 'sign1').setScale(.3);
   this.add
   .sprite(1200, 350, 'billboardmemories')
   .setDisplaySize(960, 960)
@@ -83,18 +92,18 @@ gameScene.create = function() {
   .sprite(5350, 350, 'billboardvampire')
   .setDisplaySize(960, 960)
   .setScale(.6)
-  this.button = this.physics.add.sprite(1900,575, 'button').setScale(3)
+  this.button = this.physics.add.sprite(1800,575, 'button').setScale(3)
   this.button.body.allowGravity = false;
   this.button.body.immovable = true;
   
   this.zone = new Phaser.Geom.Rectangle(this.button.x - 25, this.button.y -5, 50, 50);
-  
+  this.sentra = this.add.sprite(2700, 530, 'sentra').setScale(.5);
   this.xterra = this.physics.add.sprite(-180, 519, 'xterra').setScale(.3);
   this.xterra.body.allowGravity = false;
   this.xterra.body.immovable = true;
-  this.microtable = this.add.sprite(1800,545,'microtable').setScale(.1)
-  this.skeleton = this.add.sprite(1700,545,'skeleton').setScale(.1)
-  this.guy = this.add.sprite(2000,545,'guy')
+  this.microtable = this.add.sprite(1700,545,'microtable').setScale(.1)
+  this.skeleton = this.add.sprite(1650,545,'skeleton').setScale(.1)
+  this.guy = this.add.sprite(1900,545,'guy')
   this.guy.flipX = true
   //this.plants = this.add.tileSprite(400, 279, 800, 600, 'plants');
   //this.plants.scrollFactorX = 0;
@@ -113,7 +122,8 @@ gameScene.create = function() {
   this.player = this.physics.add.sprite(150, 300, 'player', 2);
   this.player.canAttack = true;
   this.bird = this.add.sprite(350, 300, 'bird', 0).setScale(.09);
-  this.dino = this.add.sprite(400, 533, 'dino', 0).setScale(.7);
+  this.dino = this.add.sprite(1570, 533, 'dino', 0).setScale(.7);
+  this.pikachu = this.add.sprite(3200,563,'pikachu')
   this.anims.create({
     key: 'left',
     frames: this.anims.generateFrameNumbers('player', { start: 0, end: 1 }),
@@ -146,26 +156,35 @@ gameScene.create = function() {
     key: 'dinoBreath',
     frames: this.anims.generateFrameNumbers('dino', { start: 0, end: 2 }),
     frameRate: 2,
-    repeat: -1
+    repeat: -1,
+    yoyo: true
   });
+  this.anims.create({
+    key: 'pikaStand',
+    frames: this.anims.generateFrameNumbers('pikachu', { start: 0, end: 6 }),
+    frameRate: 5,
+    repeat: -1,
+    yoyo: true
+  });
+  this.pikachu.anims.play('pikaStand')
   this.dino.anims.play('dinoBreath');
   
   this.speech = 
   [
-    this.bubble = this.add.sprite(2025, 460, 'bubble').setScale(.2).setVisible(false),
-    this.add.text(2000, 450, "Chongo", {
+    this.bubble = this.add.sprite(1925, 460, 'bubble').setScale(.2).setVisible(false),
+    this.add.text(1870, 430, "Can I walk you \n to your car?", {
     font: "16px Arial",
     fill: "black",
     align: "center",
   }).setShadow(3, 3, 'rgba(0,0,0,0.5)', 5).setVisible(false),
-  this.bubble = this.add.sprite(200, 200, 'bubble').setScale(.2).setVisible(false),
-  this.add.text(224, 224, "Trango", {
-    font: "24px Arial",
-    fill: "red",
+  this.bubble1 = this.add.sprite(1770, 460, 'bubble',).setScale(.3).setVisible(false),
+  this.add.text(1690, 420, "Have my number while \n you're at it!", {
+    font: "16px Arial",
+    fill: "purple",
     align: "center",
   }).setShadow(3, 3, 'rgba(0,0,0,0.5)', 5).setVisible(false)
   ]
-
+  this.bubble1.flipX = true
   this.input.keyboard.on('keydown_E', (event) => {
     if (Phaser.Geom.Rectangle.Overlaps(this.player.getBounds(), this.zone )) {
       this.speech.forEach(function(element){
@@ -194,7 +213,7 @@ gameScene.create = function() {
 };
 
 gameScene.update = function() {
-
+  
   this.cameras.main.scrollX = this.player.x - 400;
 
   this.background.tilePositionX = this.cameras.main.scrollX * 0.08;
